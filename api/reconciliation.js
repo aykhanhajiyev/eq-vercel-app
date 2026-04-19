b8f9e28f2a6ce03bee7c20c51b06d17c4582eb72
004476 <004476@ibar.int>
2026-04-19 22:51:14 +0400

    Revert debt row details to previous behavior

--- api/reconciliation.js
+++ api/reconciliation.js
@@ -287,13 +287,13 @@ function buildRows(eqData, bankData) {
       } else if (updatedRowsById.has(id)) {
         const updated = updatedRowsById.get(id);
         merged.push(updated);
-        if (updated._remainingPrincipal > EPS || updated._remainingVat > EPS) {
+        if (updated.status === STATUS.PARTIAL && (updated._remainingPrincipal > EPS || updated._remainingVat > EPS)) {
           merged.push({
-            reklamYayicisi: updated.reklamYayicisi || '',
-            voen: updated.voen || '',
-            icazeNo: updated.icazeNo || '',
-            eqTarixi: updated.eqTarixi || '',
-            eqNomresi: updated.eqNomresi || '',
+            reklamYayicisi: '',
+            voen: '',
+            icazeNo: '',
+            eqTarixi: '',
+            eqNomresi: '',
             eqMeblegEsas: updated._remainingPrincipal > EPS ? updated._remainingPrincipal : 0,
             eqMeblegEdv: updated._remainingVat > EPS ? updated._remainingVat : 0,
             odenisTarixi: '',
